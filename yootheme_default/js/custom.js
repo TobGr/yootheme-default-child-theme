266fe74c25a034bfb0d871500e84512ab1a054ef
TobGr <tobgr@protonmail.com>
2024-02-06 12:05:14 +0100

    files added

---
+++ yootheme_default/js/custom.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+	console.log("custom.js loaded");
+	
+	registerIcon();
+
+	// custom uikit icons
+	function registerIcon() {
+		// UIkit.icon.add('name-icon', '<svg></svg>');
+	}
+});
